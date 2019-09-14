a72443f14fe4b7e36169d730dd9a8ce6dc6f8d54
Kent VanderVelden <kent.vandervelden@gmail.com>
2019-09-14 15:38:49 -0500

    Implemented the center-of-hole finder

--- linuxcnc_driver.py
+++ linuxcnc_driver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Goal: Use machine vision to read a Haimer 3d Taster and control LinuxCNC
 # find the center of a hole.
@@ -29,13 +29,13 @@
 
 
 # Documentation for external libraries
-# http://linuxcnc.org/docs/2.6/html/common/python-interface.html
 # http://linuxcnc.org/docs/2.7/html/config/python-interface.html
 
 
 import datetime
 import sys
 import time
+import math
 
 import haimer_camera
 import linuxcnc
@@ -45,14 +45,15 @@ cnc_c = None
 
 
 def move_to(x, y, z):
-    cmd = 'G1 G54 X{0:f} Y{1:f} Z{2:f} f5'.format(x, y, z)
+    #cmd = 'G1 G54 X{0:f} Y{1:f} Z{2:f} f5'.format(x, y, z)
+    cmd = 'G1 G53 X{0:f} Y{1:f} Z{2:f} f2'.format(x, y, z)
     print('Command,' + cmd)
 
     cnc_c.mdi(cmd)
-    rv = cnc_c.wait_complete(60)
-    if rv != 1:
-        print('MDI command timed out')
-        sys.exit(1)
+#    rv = cnc_c.wait_complete(60)
+#    if rv != 1:
+#        print('MDI command timed out')
+#        sys.exit(1)
 
 
 # generate grid points in a zig-zag rectilinear pattern, helper function
@@ -98,10 +99,136 @@ def gen_grid(s, e, d):
     return grid_pts
 
 
+# From
+# http://linuxcnc.org/docs/2.7/html/config/python-interface.html
+def ok_for_mdi(s):
+        s.poll()
+        return not s.estop and s.enabled and (s.homed.count(1) == s.axes) and (s.interp_state == linuxcnc.INTERP_IDLE)
+
+
+def find_center_of_hole(video_capture):
+    state = 1
+    d1 = None
+
+    while True:
+        s = linuxcnc.stat()
+        s.poll()
+        print(s.axis[0]['input'], s.axis[0]['output'], s.axis[0]['inpos'], s.axis[0]['homed'], s.axis[0]['velocity'], s.axis[0]['enabled'])
+
+        x = s.axis[0]['inpos']
+        y = s.axis[1]['inpos']
+        z = s.axis[2]['inpos']
+        inpos = x == 1 and y == 1 and z == 1
+
+        x = s.axis[0]['input']
+        y = s.axis[1]['input']
+        z = s.axis[2]['input']
+
+        mm_final = haimer_camera.get_measurement(video_capture)
+        if mm_final is None:
+            print('mm_final is None')
+            continue
+        in_final = mm_final / 25.4
+
+        if mm_final > 1.0:
+            print('Danger!')
+            cnc_c.abort()
+            state = -1
+            continue
+
+        #tar_x = -1.20
+        #xe = x - tar_x
+
+        xe = 0
+        ye = 0
+
+        cmd_x = x
+        cmd_y = y
+        cmd_z = z
+
+        if state == -1:
+            continue
+        if state == 1:
+            tar_x = 0
+            xe = in_final - tar_x
+
+            xe *= -1
+            cmd_x = x - xe/2
+        elif state == 2:
+            tar_x = 0
+            xe = in_final - tar_x
+
+            cmd_x = x - xe/2
+        elif state == 3:
+            xe = x - tar_x
+            if abs(xe) > .1:
+                xe = math.copysign(.1, xe)
+
+            cmd_x = x - xe/2
+        elif state == 4:
+            tar_y = 0
+            ye = in_final - tar_y
+
+            ye *= -1
+            cmd_y = y - ye/2
+        elif state == 5:
+            tar_y = 0
+            ye = in_final - tar_y
+
+            cmd_y = y - ye/2
+        elif state == 6:
+            ye = y - tar_y
+            if abs(ye) > .1:
+                ye = math.copysign(.1, ye)
+
+            cmd_y = y - ye/2
+            print('state 6', aft_y, forward_y, tar_y, y, ye, cmd_y)
+
+        total_e = abs(xe) + abs(ye)
+    #    print('mm_final:', mm_final, 'in_final:', in_final, 'cur_x:', x, 'tar_x:', tar_x, 'xe:', xe, 'cmd_x:', cmd_x, inpos, ok_for_mdi(s))
+  
+        if ok_for_mdi(s) and inpos:
+            if total_e > .0005:
+                if d1 is not None and time.time() - d1 < .5:
+                    print('In settling period')
+                    continue
+                move_to(cmd_x, cmd_y, cmd_z)
+                d1 = time.time()
+            else:
+                print('No move is needed')
+                if state == 1:
+                    left_x = x
+                    cmd_x += .1
+                    move_to(cmd_x, y, z)
+                    state = 2
+                elif state == 2:
+                    right_x = x
+                    tar_x = left_x + (right_x - left_x) / 2.
+                    state = 3
+                elif state == 3:
+                    state = 4
+                elif state == 4:
+                    aft_y = y
+                    cmd_y += .1
+                    move_to(x, cmd_y, z)
+                    state = 5
+                elif state == 5:
+                    forward_y = y
+                    tar_y = forward_y + (aft_y - forward_y) / 2.
+                    state = 6
+                elif state == 6:
+                    print('Done, center found at:', cmd_x, cmd_y, cmd_z)
+                    break
+        else:
+            print('Waiting for last move to complete.')
+
+        sys.stdout.flush()
+
+
 def main():
-    if len(sys.argv) != 1 + 3 * 3:
-        print(f'usage: {sys.argv[0]} <xs> <xe> <xd>  <ys> <ye> <yd>  <zs> <ze> <zd>')
-        sys.exit(1)
+#    if len(sys.argv) != 1 + 3 * 3:
+#        print(f'usage: {sys.argv[0]} <xs> <xe> <xd>  <ys> <ye> <yd>  <zs> <ze> <zd>')
+#        sys.exit(1)
 
     args = map(float, sys.argv[1:])
 
@@ -114,26 +241,28 @@ def main():
     cnc_c.mode(linuxcnc.MODE_MDI)
     cnc_c.wait_complete()
 
-    s = args[0::3]
-    e = args[1::3]
-    d = args[2::3]
+#    s = args[0::3]
+#    e = args[1::3]
+#    d = args[2::3]
 
     video_capture = haimer_camera.gauge_vision_setup()
     # Warm up
     for i in range(30):
         mm_final = haimer_camera.get_measurement(video_capture)
 
-    grid_pts = gen_grid(s, e, d)
-    for pp in grid_pts:
-        z, y, x = pp
-        move_to(x, y, z)
-        time.sleep(.1)  # settling time
+    find_center_of_hole(video_capture)
 
-        mm_final = haimer_camera.get_measurement(video_capture)
-        print('mm_final:', mm_final)
-
-        dt = str(datetime.datetime.now())
-        print('Result,' + ','.join(map(str, [dt, x, y, z, mm_final])), flush=True)
+    #grid_pts = gen_grid(s, e, d)
+    #for pp in grid_pts:
+    #    z, y, x = pp
+    #    move_to(x, y, z)
+    #    time.sleep(.1)  # settling time
+#
+#
+#        dt = str(datetime.datetime.now())
+#        #print('Result,' + ','.join(map(str, [dt, x, y, z, mm_final])), flush=True)
+#        print('Result,' + ','.join(map(str, [dt, x, y, z, mm_final])))
+#        sys.stdout.flush()
 
 
 if __name__ == "__main__":
